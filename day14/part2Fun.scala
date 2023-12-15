45fca7c9c0bb5b096301259cff40a359da9e8c67
coachDave <salathe.david@gmail.com>
2023-12-15 09:55:38 +0100

    cleaned part 2 day 14 Functional Version

--- day14/part2Fun.scala
+++ day14/part2Fun.scala
@@ -1,22 +1,15 @@
-//> using platform "scala-native"
-//> using nativeMode "release-full"
-//> using nativeGc "none"
-
 import io.Source
 
 object Part2Fun {
-
   val N_CYCLES = 1_000_000_000
 
   def main(args: Array[String]): Unit =
     val platform = parseInput(args(0))
     val (cycleLength, startingLength) = hareAndTortoise(platform)
-    println(s"$cycleLength $startingLength")
     val simulationNumber = ((N_CYCLES - startingLength) % cycleLength) + startingLength
     val res = (1 to simulationNumber).foldLeft(platform)((p, _) => p.spin())
     println(res.countWeight())
  
-
   def hareAndTortoise(start: Platform): (Int, Int) =
     def run(hare: Platform, tortoise: Platform, currentCount: Int=0): (Platform, Int) =
       if hare == tortoise then
@@ -29,7 +22,6 @@ object Part2Fun {
     val (_, lambda) = run(secondEncounter.spin(), secondEncounter, 1)
     (lambda, mu)
 
-
   def parseInput(s: String): Platform =
     Platform(Source.fromFile(s).getLines().filter(!_.isBlank()).map(_.toVector).toVector)
 
@@ -37,15 +29,9 @@ object Part2Fun {
       case N, W, S, E
   
   case class Platform(grid: Vector[Vector[Char]]):
-    import scala.collection.mutable.ArrayBuffer
+    def countWeight(): Int = grid.zipWithIndex.map { case (line, i) => line.count(_ == 'O') * (grid.size - i) }.sum
 
-    def countWeight(): Int =
-      val size = grid.size
-      grid.zipWithIndex.map { case (line, i) => line.count(_ == 'O') * (size - i) }.sum
-
-    def spin(): Platform =
-      val mutableGrid = grid.map(_.to(ArrayBuffer)).to(ArrayBuffer)
-      Direction.values.foldLeft(this)((p, d) => p.tilt(d))
+    def spin(): Platform = Direction.values.foldLeft(this)((p, d) => p.tilt(d))
 
     def tilt(direction: Direction): Platform =
       val transposedGrid = if direction == Direction.N || direction == Direction.S then grid.transpose else grid
@@ -59,7 +45,5 @@ object Part2Fun {
       }.toVector
       val transposedNewGrid = if direction == Direction.N || direction == Direction.S then newGrid.transpose else newGrid
       Platform(transposedNewGrid)
-      
-    override def toString(): String = grid.map(_.mkString).mkString("\n")
 
 }
